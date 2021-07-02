0ee1b8343497a32427056a3b75b8ba025e778d7c
Geektree0101 <h2s1880@gmail.com>
2021-07-02 10:12:45 +0900

    create main.go

--- annohook/main.go
+++ annohook/main.go
@@ -1,6 +1,37 @@
 package main
 
+import (
+	"flag"
+	"os"
+)
+
+var projectBaseDIR string
+var sourceFileType string
+
+var slackHookURL string
+var slackHookImage string
+var slackHookName string
+
+func init() {
+	flag.StringVar(&projectBaseDIR, "projectBaseDIR", "", "project base dir")
+	flag.StringVar(&sourceFileType, "sourceFileType", "", "source file type (ex. swift)")
+	flag.StringVar(&slackHookURL, "slackHookURL", "", "slack hook url")
+	flag.StringVar(&slackHookImage, "slackHookImage", "", "slack hook image")
+	flag.StringVar(&slackHookName, "slackHookName", "" , "slack hook name")
+	flag.Parse()
+}
+
+func invalidArgs() {
+	flag.Usage()
+	os.Exit(-1)
+}
+
 func main() {
 
+	if len(os.Args) < 2 || len(projectBaseDIR) == 0 || len(sourceFileType) == 0 {
+		invalidArgs()
+		return
+	}
+	
 	// TODO
 }
